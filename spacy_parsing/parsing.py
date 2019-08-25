67f3c7c89b4ab7d098b397bced651bd9ea56a664
Szymon Rutkowski <szymon@szymonrutkowski.pl>
2019-08-25 19:32:41 +0200

    big parse of isolation-related utterances for further testing

--- spacy_parsing/parsing.py
+++ spacy_parsing/parsing.py
@@ -1,11 +1,14 @@
 from spacy_conll import Spacy2ConllParser
+import json
 
 spacyconll = Spacy2ConllParser(model='en_core_web_lg')
 
-# TODO read from Redis.
-with open('input_utterances.txt') as utt_file:
-    for utterance in utt_file.readlines():
-        if utterance.strip() == '':
+#with open('input_utterances.txt') as utt_file:
+#    for utterance in utt_file.readlines():
+with open('/home/szymon/lingwy/therminsley/sitesdb/1970/raw_utterances.json') as utt_json_file:
+    utterances = json.load(utt_json_file)
+    for utterance in utterances:
+        if utterance.strip() == '' or not ('solation' in utterance):
             continue
         for parsed_sent in spacyconll.parse(input_str=utterance):
             print(parsed_sent)
