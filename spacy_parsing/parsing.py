8b017773a7195c5defd72752e2ee456fd69e000b
Szymon Rutkowski <szymon@szymonrutkowski.pl>
2019-08-27 21:51:24 +0200

    trying to get all (more) utterances

--- spacy_parsing/parsing.py
+++ spacy_parsing/parsing.py
@@ -8,8 +8,9 @@ spacyconll = Spacy2ConllParser(model='en_core_web_lg')
 with open('/home/szymon/lingwy/therminsley/sitesdb/1970/raw_utterances.json') as utt_json_file:
     utterances = json.load(utt_json_file)
     for utterance in utterances:
-        if utterance.strip() == '' or not ('solation' in utterance):
-            continue
+        # this changes nothing in practice
+        ##if utterance.strip() == '' or not ('solation' in utterance):
+        ##    continue
         for parsed_sent in spacyconll.parse(input_str=utterance):
             print(parsed_sent)
             print()
