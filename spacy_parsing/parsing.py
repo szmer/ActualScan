2882fc511cdc2057ac250136c13fe84c78b4d3b8
Szymon Rutkowski <szymon@szymonrutkowski.pl>
2019-08-29 00:09:09 +0200

    parsing with paragraph separation

--- spacy_parsing/parsing.py
+++ spacy_parsing/parsing.py
@@ -8,11 +8,12 @@ spacyconll = Spacy2ConllParser(model='en_core_web_lg')
 with open('/home/szymon/lingwy/therminsley/sitesdb/1970/raw_utterances.json') as utt_json_file:
     utterances = json.load(utt_json_file)
     for utterance in utterances:
-        print('#####')
-        # this changes nothing in practice
-        ##if utterance.strip() == '' or not ('solation' in utterance):
-        ##    continue
-        for parsed_sent in spacyconll.parse(input_str=utterance):
-            print(parsed_sent)
-            print()
-        print()
+        for paragraph in utterance.split('\n'):
+            if len(paragraph.strip()) == 0:
+                continue
+            print('#####')
+            # this changes nothing in practice
+            ##if utterance.strip() == '' or not ('solation' in utterance):
+            ##    continue
+            for parsed_sent in spacyconll.parse(input_str=paragraph):
+                print(parsed_sent)
