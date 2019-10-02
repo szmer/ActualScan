077dce8d782db44b55d7c648fe22af150b521ece
Szymon Rutkowski <szymon@szymonrutkowski.pl>
2019-10-02 23:35:23 +0200

    ignore "dead" page rows in spacy parsing

--- spacy_parsing/parsing.py
+++ spacy_parsing/parsing.py
@@ -1,15 +1,20 @@
 from spacy_conll import Spacy2ConllParser
+import os
 import json
 
 spacyconll = Spacy2ConllParser(model='en_core_web_lg')
 
+# CONFIG desired range of rows here.
 #ids = [1970]
 ids = range(1938)
 
 #with open('input_utterances.txt') as utt_file:
 #    for utterance in utt_file.readlines():
 for page_id in ids:
-    with open('/home/szymon/lingwy/therminsley/sitesdb/'+str(page_id)+'/raw_utterances.json') as utt_json_file:
+    # (skip files marked as dead - ie. duplicates)
+    if os.path.isfile('~/therminsley/sitesdb/'+str(page_id)+'/dead'):
+        continue
+    with open('~/therminsley/sitesdb/'+str(page_id)+'/raw_utterances.json') as utt_json_file:
         utterances = json.load(utt_json_file)
         for utterance in utterances:
             for paragraph in utterance.split('\n'):
